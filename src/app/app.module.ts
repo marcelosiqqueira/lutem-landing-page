5bc9651cd482c01563f08ab552bcc4092369a6a0
Marcelo Siqueira <marcelosiqqueira@gmail.com>
2024-05-01 18:58:24 -0300

    feat: implements routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { InitialCardComponent } from './components/initial-card/initial-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuBarComponent,
+    InitialCardComponent
   ],
   imports: [
     BrowserModule,
