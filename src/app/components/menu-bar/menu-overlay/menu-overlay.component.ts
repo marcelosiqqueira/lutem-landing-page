636feb73adf32e54d7886db9a7f1c0aeb51280a4
Marcelo Siqueira <marcelosiqqueira@gmail.com>
2024-06-29 14:26:05 -0300

    fix: overflow y hidden in menu overlay

--- src/app/components/menu-bar/menu-overlay/menu-overlay.component.ts
+++ src/app/components/menu-bar/menu-overlay/menu-overlay.component.ts
@@ -1,15 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-overlay',
   templateUrl: './menu-overlay.component.html',
   styleUrls: ['./menu-overlay.component.scss']
 })
-export class MenuOverlayComponent {
+export class MenuOverlayComponent implements OnInit, OnDestroy{
   @Input() isMenuOpen = false;
+
+  preventScroll = (event: TouchEvent) => {
+    event.preventDefault();
+  }
+
+  ngOnInit() {
+    
+  }
+
+  ngOnDestroy() {
+    document.body.style.overflow = '';
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+
+    if (this.isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+      document.body.addEventListener('touchmove', this.preventScroll, { passive: false });
+    } else {
+      document.body.style.overflow = '';
+      document.body.removeEventListener('touchmove', this.preventScroll);
+    }
   }
 
   scrollToSection(sectionId: string) {
